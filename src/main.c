a508fcc2c7ee0c46639aba189bf2847d1c4f383e
gauranga18 <sauravjyotikalita56@gmail.com>
2025-12-21 02:05:38 +0530

    Added cloning feature on main

--- src/main.c
+++ src/main.c
@@ -1,24 +1,38 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include "utils.h"
+
 int main(int argc, char *argv[]) {
-    // printf("argc = %d\n",argc);
-    // if(argc<3){
-    //     printf("Usage: tool deploy <repo_url>\n");
-    //     return 1;
-    // }
-    // if(strcmp(argv[1],"deploy")!=0){
-    //     printf("Unknown Command: %s\n",argv[1]);
-    //     return 1;
-    // }
-    // char *repo_url = argv[2];
-    // printf("Starting deployment for: %s\n", repo_url);
-    // for(int i=0; i<argc;i++){
-    //     printf("argv[%d] = %s\n",i ,argv[i]);    
-    // }    
-    run_command("git --version");
-    run_command("ls");
-    run_command("pwd");
-    run_command("git clone http://github.com/gauranga18/forge.git");
+
+    /* 1. Validate argument count */
+    if (argc < 3) {
+        printf("Usage: %s deploy <repo_url>\n", argv[0]);
+        return 1;
+    }
+
+    /* 2. Validate command */
+    if (strcmp(argv[1], "deploy") != 0) {
+        printf("Unknown command: %s\n", argv[1]);
+        printf("Usage: %s deploy <repo_url>\n", argv[0]);
+        return 1;
+    }
+
+    /* 3. Extract repo URL */
+    const char *repo_url = argv[2];
+
+    /* 4. Build git clone command */
+    char cmd[256];
+    snprintf(cmd, sizeof(cmd), "git clone %s", repo_url);
+
+    /* 5. Execute command */
+    int ret = run_command(cmd);
+
+    /* 6. Handle failure */
+    if (ret != 0) {
+        printf("Deployment failed :( \n");
+        return 1;
+    }
+
+    printf("Repository cloned successfully :) \n");
     return 0;
 }
